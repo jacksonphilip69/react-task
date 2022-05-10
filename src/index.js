a3b6afff18e0d7398b25d32a7344c058ec93fcd3
jackson <jacksonphilip1874511@gmail.com>
2022-05-10 18:28:17 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Two extends React.Component {
+constructor(props){
+super(props);
+this.state={value:''}
+    this.handleChange = this.handleChange.bind(this);
+}
+handleChange(event) {
+    this.setState({value: event.target.value})
+}  
+render() {
+  return (
+    <div>
+      <header className="box">
+        <form>
+          <textarea onChange={this.handleChange} name='name' value={this.state.value}></textarea>
+          <textarea value={this.state.value}></textarea>
+        </form>
+      </header>
+    </div>
+    )
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Two/>);
